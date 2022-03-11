e6c6963fda774cb46acce31a20cde490c8e8d2ab
Marina Golubeva <golubeva.webmaster@gmail.com>
2022-03-11 11:48:01 +0300

    reorder

---
+++ costs/src/components/NewCost/CostForm.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import "./CostForm.css";
+
+const CostForm = (props) => {
+  const [inputName, setInputName] = useState("");
+  const [inputAmount, setInputAmount] = useState("");
+  const [inputDate, setInputDate] = useState("");
+
+  // const [userInput, setUserInput] = useState({
+  //   name: "",
+  //   amount: "",
+  //   date: "",
+  // });
+
+  const nameChangeHandler = (event) => {
+    setInputName(event.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   name: event.target.value,
+    // });
+    // setUserInput((previouseState) => { // самый последний стейт
+    //   return {
+    //     ...previouseState,
+    //     name: event.target.value,
+    //   };
+    // });
+  };
+
+  const amountChangeHandler = (event) => {
+    setInputAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   amount: event.target.value,
+    // });
+  };
+
+  const dateChangeHandler = (event) => {
+    setInputDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   date: event.target.value,
+    // });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const costDate = {
+      description: inputName,
+      amount: inputAmount,
+      date: new Date(inputDate),
+    };
+
+    props.onSaveCostData(costDate);
+    setInputName("");
+    setInputAmount("");
+    setInputDate("");
+    props.onCancelCostData();
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <div className="new-cost__controls">
+          <div className="new-cost__control">
+            <label>Название</label>
+            <input type="text" value={inputName} onChange={nameChangeHandler} />
+          </div>
+          <div className="new-cost__control">
+            <label>Сумма</label>
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              value={inputAmount}
+              onChange={amountChangeHandler}
+            />
+          </div>
+          <div className="new-cost__control">
+            <label>Дата</label>
+            <input
+              type="date"
+              min="2020-01-01"
+              max="2023-11-31"
+              step="1"
+              value={inputDate}
+              onChange={dateChangeHandler}
+            />
+          </div>
+          <div className="new-cost__actions">
+            <button type="submit">Добавить расход</button>
+            <button type="button" onClick={props.onCancelCostData}>
+              Отменить
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CostForm;
